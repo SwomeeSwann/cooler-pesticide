e3777d768322a77b38a57318e61564bf3e2e542e
SwomeeSwann <157737664+SwomeeSwann@users.noreply.github.com>
2024-03-06 07:06:21 -0600

    Add files via upload

--- farmingScript.gd
+++ farmingScript.gd
@@ -1,5 +1,6 @@
 extends TileMap
 
+
 @onready var parentNode = get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,5 +16,5 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and get_parent() == parentNode:
 			var tilePos = local_to_map(event.position)
-			if not tilePos.y == 0:
+			if 0 < tilePos.y and tilePos.y < 13:
 				set_cell(0, tilePos, 1, Vector2(0,0))
